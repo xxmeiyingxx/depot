9fbaeb87ab871ed14a76f8c8cf3a490c073eea79
Sadahiro Ikeuchi <meiying.ruby@gmail.com>
2015-11-11 05:22:10 +0000

    Validation!

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110011422) do
+ActiveRecord::Schema.define(version: 20151110060132) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price",       precision: 8, scale: 2
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.decimal  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
